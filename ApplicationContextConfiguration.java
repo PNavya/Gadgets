9fd87add80f1941d8958467db436c5c7e5992726
P.Navya <pallenavya@gmail.com>
2016-08-11 13:02:50 +0530

    Add files via upload

---
+++ ApplicationContextConfiguration.java
@@ -0,0 +1,64 @@
+package com.niit.ShoppingCart.configuration;
+
+	import java.util.Properties;
+	 
+	import javax.sql.DataSource;
+	 
+	import org.hibernate.SessionFactory;
+	import org.springframework.beans.factory.annotation.Autowired;
+	import org.springframework.context.annotation.Bean;
+	import org.springframework.context.annotation.ComponentScan;
+	import org.springframework.context.annotation.Configuration;
+	import org.springframework.context.annotation.PropertySource;
+	import org.springframework.core.env.Environment;
+	import org.springframework.jdbc.datasource.DriverManagerDataSource;
+	import org.springframework.orm.hibernate4.HibernateTransactionManager;
+	import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
+	import org.springframework.transaction.annotation.EnableTransactionManagement;
+	 
+	@Configuration
+	@EnableTransactionManagement
+	@ComponentScan({ "com.niit.ShoppingCart" })
+	@PropertySource(value = { "classpath:application.properties" })
+	public class ApplicationContextConfiguration {
+	 
+	    @Autowired
+	    private Environment environment;
+	 
+	    @Bean
+	    public LocalSessionFactoryBean sessionFactory() {
+	        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+	        sessionFactory.setDataSource(dataSource());
+	        sessionFactory.setPackagesToScan(new String[] { "com.niit.ShoppingCart" });
+	        sessionFactory.setHibernateProperties(hibernateProperties());
+	        return sessionFactory;
+	     }
+	     
+	    @Bean
+	    public DataSource dataSource() {
+	        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+	        dataSource.setDriverClassName(environment.getRequiredProperty("org.h2.driver"));
+	        dataSource.setUrl(environment.getRequiredProperty("jdbc:h2:tcp://localhost/~/navya"));
+	        dataSource.setUsername(environment.getRequiredProperty("sa"));
+	        dataSource.setPassword(environment.getRequiredProperty(""));
+	        return dataSource;
+	    }
+	     
+	    private Properties hibernateProperties() {
+	        Properties properties = new Properties();
+	        properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
+	        properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
+	        properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
+	        return properties;        
+	    }
+	     
+	    @Bean
+	    @Autowired
+	    public HibernateTransactionManager transactionManager(SessionFactory s) {
+	       HibernateTransactionManager txManager = new HibernateTransactionManager();
+	       txManager.setSessionFactory(s);
+	       return txManager;
+	    }
+	}
+
+
